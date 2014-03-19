4ddd979464594b1f2c6143eca88ed9e535059544
matematik7 <domen.ipavec@z-v.si>
2014-03-19 22:41:30 +0100

    Shows /proc/cpuinfo and cputemp

---
+++ main.c
@@ -0,0 +1,43 @@
+
+#include "webservice.h"
+
+#include <stdlib.h>
+
+DUDA_REGISTER("Duda IO test", "Test");
+
+void cb_info(duda_request_t *dr)
+{
+	response->http_status(dr, 200);
+
+	FILE *cmdline = fopen("/proc/cpuinfo", "rb");
+	char *arg = 0;
+	size_t size = 0;
+	while(getdelim(&arg, &size, 0, cmdline) != -1)
+	{
+		response->printf(dr, arg);
+	}
+	free(arg);
+	fclose(cmdline);
+	
+	response->end(dr, NULL);
+}
+
+void cb_cputemp(duda_request_t *dr)
+{
+	response->http_status(dr, 200);
+	
+	int i = response->sendfile(dr, "/sys/class/thermal_zone0/temp");
+	if (i != 0) {
+		response->printf(dr, "ERROR");
+	}
+	
+	response->end(dr, NULL);
+}
+
+int duda_main()
+{
+	map->static_add("/information", "cb_info");
+	map->static_add("/cputemp", "cb_cputemp");
+	
+	return 0;
+}
